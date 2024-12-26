62ce0bff6fdadc5d5e7e9358bfc8f8c38024eb9a
JoshuaCho021212 <j8cho@uwaterloo.ca>
2024-12-25 19:58:51 -0500

    Update modified_spaceship.py

--- modified_spaceship.py
+++ modified_spaceship.py
@@ -1,166 +1,174 @@
 import pygame
 import random
-import sys
 from time import sleep
+import sys
 
-# Game Settings
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 640
-SHUTTLE_SIZE = (53, 111)  # width, height
-ASTEROID_SIZE = (39, 22)  # width, height
-MISSILE_LIMIT = 2
-DESTROY_COUNT = 0
-SHUTTLE_LIVES = 3
 
-# Initialize Game
-def initialize():
-    global screen, clock, shuttle_img, missile_img, asteroid_img, sound_shot, sound_explosion, sound_destroy
+#variable
+screen_width = 480
+screen_height = 640
+shuttle_width = 53
+shuttle_height = 111
+asteroid_width = 39
+asteroid_height = 22
+d_count = 0
+s_num = 3
+
+# reset the game
+def startGame():
+    global screen, clock, shuttle, missile, asteroid, s_shot, s_explode, s_destroy
 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Defender of Earth")
-
-    # Load images and sounds
-    shuttle_img = pygame.image.load('shuttle.jpg')
-    asteroid_img = pygame.image.load('ast.png')
-    missile_img = pygame.image.load('mis.png')
-    sound_shot = pygame.mixer.Sound('shot.wav')
-    sound_explosion = pygame.mixer.Sound('big.wav')
-    sound_destroy = pygame.mixer.Sound('small.wav')
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption('Saving Earth')
+    shuttle = pygame.image.load('shuttle.jpg')
+    asteroid = pygame.image.load('ast.png')
+    missile = pygame.image.load('mis.png')
+    s_shot = pygame.mixer.Sound('shot.wav')
+    s_explode = pygame.mixer.Sound('big.wav')
+    s_destroy = pygame.mixer.Sound('small.wav')
     clock = pygame.time.Clock()
 
-# Draw Object on Screen
-def draw_image(image, x, y):
-    screen.blit(image, (x, y))
+# include object
+def drawObject(obj, x, y):
+    global screen
+    screen.blit(obj, (x, y))
 
-# Handle Collision
-def handle_collision():
+# collision
+def explode():
     pygame.display.update()
     sleep(3)
-    main_game_loop()
+    runGame()
 
-# Display Current Score
-def display_score(score):
+# printing score
+def showScore(count):
+    global screen
     font = pygame.font.SysFont('malgungothic', 20)
-    score_text = font.render(f"SCORE: {score}", True, (0, 0, 255))
-    screen.blit(score_text, (10, 10))
+    text = font.render("SCORE: " + str(count),True, (0, 0, 255))
+    screen.blit(text, (0,0))
 
-# End the Game
-def end_game():
+# game over
+def gameOver():
+    global screen
     font = pygame.font.SysFont('malgungothic', 50)
-    end_text = "Mission Complete!" if DESTROY_COUNT == 100 else "Game Over!"
-    text_color = (0, 255, 0) if DESTROY_COUNT == 100 else (255, 0, 0)
-    
-    # Render text
-    rendered_text = font.render(end_text, True, text_color)
-    
-    # Get the width and height of the rendered text
-    text_width = rendered_text.get_width()
-    text_height = rendered_text.get_height()
-    
-    # Center the text on the screen
-    text_x = (SCREEN_WIDTH - text_width) // 2
-    text_y = (SCREEN_HEIGHT - text_height) // 2
-    
-    # Display text
-    screen.blit(rendered_text, (text_x, text_y))
+   if d_count == 100:
+        text = font.render("Misssion Complete!", True, (0, 255, 0))
+        screen.blit(text, (screen_width/2-210, screen_height/2-30))
+    else:    
+        text = font.render("Game Over!",True, (255, 0, 0))
+        screen.blit(text, (screen_width/2-150, screen_height/2-30))
     
     pygame.display.update()
     sleep(2)
-    main_game_loop()
+    runGame()
+    
 
-# Main Game Loop
-def main_game_loop():
-    global DESTROY_COUNT, SHUTTLE_LIVES
+#function of starting game
+def runGame():
+    global d_count, s_num, s_shot, s_explode, s_destroy
 
-    missiles = []
-    x_pos = SCREEN_WIDTH * 0.4
-    y_pos = SCREEN_HEIGHT * 0.75
+    #generating list of misiles
+    missile_xy = []
+
+    
+
+    # coordinate of spaceship
+    x = screen_width * 0.40
+    y = screen_height * 0.75
     x_change = 0
-
-    # Initialize Asteroid
-    asteroid_x = random.randint(0, SCREEN_WIDTH - ASTEROID_SIZE[0])
+    
+    # initial location of asteroids
+    asteroid_x = random.randrange(0, screen_width - asteroid_width)
     asteroid_y = 0
     asteroid_speed = 3
-
-    game_running = True
-
-    while game_running:
+    
+    done = False
+    while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                ongame = True
                 pygame.quit()
                 sys.exit()
 
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x_change = -5
+                    x_change -= 5
                 elif event.key == pygame.K_RIGHT:
-                    x_change = 5
+                    x_change += 5
+
                 elif event.key == pygame.K_SPACE:
-                    sound_shot.play()
-                    if len(missiles) < MISSILE_LIMIT:
-                        missile_x = x_pos + SHUTTLE_SIZE[0] / 2
-                        missile_y = y_pos - SHUTTLE_SIZE[1] / 4
-                        missiles.append([missile_x, missile_y])
+                    s_shot.play()
+                    if len(missile_xy) <2:
+                        missile_x = x + shuttle_width/2
+                        missile_y = y - shuttle_height/4
+                        missile_xy.append([missile_x,missile_y])
 
-            if event.type == pygame.KEYUP:
-                if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                    x_change = 0
-
+           
         screen.fill((255, 255, 255))
 
-        # Move Shuttle
-        x_pos += x_change
-        if x_pos < 0:
-            x_pos = 0
-        elif x_pos > SCREEN_WIDTH - SHUTTLE_SIZE[0]:
-            x_pos = SCREEN_WIDTH - SHUTTLE_SIZE[0]
 
-        # Collision Check: Shuttle & Asteroid
-        if y_pos < asteroid_y + ASTEROID_SIZE[1]:
-            if asteroid_x > x_pos and asteroid_x < x_pos + SHUTTLE_SIZE[0]:
-                SHUTTLE_LIVES -= 1
-                sound_explosion.play()
-                handle_collision()
+        # limitation of moving spaceship
+        x += x_change
+        if x < 0:
+            x = 0
+        elif x > screen_width - shuttle_width:
+            x = screen_width - shuttle_width
 
-        if SHUTTLE_LIVES == 0 or DESTROY_COUNT == 100:
-            end_game()
+        # AABB
+        if y < asteroid_y + asteroid_height:
+            if asteroid_x > x and asteroid_x < x + shuttle_width:
+               s_num -= 1
+               s_explode.play()
+               explode()
 
-        draw_image(shuttle_img, x_pos, y_pos)
+        # CONDITION OF GAME OVER - AABB
+        if s_num == 0:
+            gameOver()
+        if d_count == 100:
+            gameOver()
+                  
+           
+                
+        drawObject(shuttle, x, y)
 
-        # Move Missiles
-        if missiles:
-            for missile in missiles[:]:
-                missile[1] -= 10
-                if missile[1] < 0:
-                    missiles.remove(missile)
-
-                # Check for Collision: Missile & Asteroid
-                if asteroid_y < missile[1] < asteroid_y + ASTEROID_SIZE[1]:
-                    if asteroid_x < missile[0] < asteroid_x + ASTEROID_SIZE[0]:
-                        sound_destroy.play()
-                        DESTROY_COUNT += 10
-                        missiles.remove(missile)
-                        asteroid_x = random.randint(0, SCREEN_WIDTH - ASTEROID_SIZE[0])
+        # movement of misiles
+        if len(missile_xy) != 0:
+            for i, bxy in enumerate(missile_xy):
+                bxy[1] -= 10
+                missile_xy[i][1] = bxy[1]
+                # removing asteroids
+                if bxy[1] < asteroid_y:
+                    if bxy[0] > asteroid_x and bxy[0] < asteroid_x + asteroid_width:
+                        missile_xy.remove(bxy)
+                        asteroid_x = random.randrange(0, screen_width-asteroid_width)
                         asteroid_y = 0
+                        d_count += 10
+                        s_destroy.play()
+                        
+              
+                if bxy[1] <= 0:
+                    try:
+                        missile_xy.remove(bxy)
+                    except:
+                        pass
+        if len(missile_xy) != 0:
+            for bx, by in missile_xy:
+                drawObject(missile, bx, by)
 
-            for missile_x, missile_y in missiles:
-                draw_image(missile_img, missile_x, missile_y)
+        showScore(d_count)       
 
-        # Update Score
-        display_score(DESTROY_COUNT)
-
-        # Move Asteroid
+        # asteroid moving
         asteroid_y += asteroid_speed
-        if asteroid_y > SCREEN_HEIGHT:
+        if asteroid_y > screen_height:
             asteroid_y = 0
-            asteroid_x = random.randint(0, SCREEN_WIDTH - ASTEROID_SIZE[0])
+            asteroid_x = random.randrange(0, screen_width - asteroid_width)
 
-        draw_image(asteroid_img, asteroid_x, asteroid_y)
-
+        drawObject(asteroid, asteroid_x, asteroid_y)   
+       
         pygame.display.update()
         clock.tick(60)
 
-# Start Game
-initialize()
-main_game_loop()
+    
+startGame()
+runGame()
